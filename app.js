1bd8da07b2f33c6af8805d9dc601693f58821c3a
Rodrigo Albano Hofer Bastos <digoalbano1010@gmail.com>
2022-04-05 23:21:14 -0300

    finished

--- app.js
+++ app.js
@@ -11,33 +11,64 @@ window.onload = () => {
     const lap = document.querySelector('.lap');
     const reset = document.querySelector('.reset');
     let interval;
+    // create div
+    const division = document.createElement('div');
+    //add class
+    division.className = "laps";
+    document.body.appendChild(division)
 
     start.onclick = function () {
-        setInterval(addTens, 10)
-        function addTens () {
-            tens++
-            if (tens <= 9) {
-                appendTens.innerHTML = "0" + tens;
-            }
-            if (tens > 9) {
-                appendTens.innerHTML = tens;
-            }
-            if (tens > 99){
-                tens = 0;
-                seconds++;
-                appendSeconds.innerHTML = "0" + seconds;
-                if (seconds > 9) {
-                    appendSeconds.innerHTML = seconds;
-                }
-                if (seconds == 60) {
-                    seconds = 00;
-                    minutes++
-                    appendMinutes.innerHTML = '0' + minutes
-                }
-            }   
-        }
+        clearInterval(interval);
+        interval = setInterval(addTens, 10)
     }
 
+    stop.onclick = function() {
+        clearInterval(interval);
+    }
+
+    lap.onclick = function() {
+        clearInterval(interval);
+        const p = document.createElement("p");
+        // add text
+        p.innerHTML = `${appendMinutes.textContent} : ${appendSeconds.textContent} : ${appendTens.textContent}`;
+        document.body.appendChild(division).appendChild(p);
+        interval = setInterval(addTens, 10);
+    }
+
+    reset.onclick = function() {
+        clearInterval(interval)
+        tens = "00";
+        seconds = "00";
+        minutes = "00";
+        appendTens.innerHTML = tens;
+        appendMinutes.innerHTML = minutes;
+        appendSeconds.innerHTML = seconds;
+        division.remove();
+        location.reload();
+    }
+
+    function addTens () {
+        tens++
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+        if (tens > 9) {
+            appendTens.innerHTML = tens;
+        }
+        if (tens > 99){
+            tens = 0;
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            if (seconds > 9) {
+                appendSeconds.innerHTML = seconds;
+            }
+            if (seconds == 60) {
+                seconds = 00;
+                minutes++
+                appendMinutes.innerHTML = '0' + minutes
+            }
+        }   
+    }
 }
 
 
